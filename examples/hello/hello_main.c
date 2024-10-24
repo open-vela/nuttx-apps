d07310389fd1697ba4c1dd69aaa911536ae22f24
xuxingliang <xuxingliang@xiaomi.com>
2024-10-24 18:23:47 +0800

    DEMO: TEST: gdb/debug: example to show GDB usage
    Change-Id: I3b4c95f911dd844d6524b9012c39ab17703faf68
    Signed-off-by: xuxingliang <xuxingliang@xiaomi.com>

--- examples/hello/hello_main.c
+++ examples/hello/hello_main.c
@@ -24,6 +24,8 @@
 
 #include <nuttx/config.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
 
 /****************************************************************************
  * Public Functions
@@ -33,8 +35,100 @@
  * hello_main
  ****************************************************************************/
 
-int main(int argc, FAR char *argv[])
-{
+static int hello(int argc, FAR char *argv[]) {
   printf("Hello, World!!\n");
+  return 0;
+}
+
+static int breakpoint(int argc, FAR char *argv[]) {
+  printf("Breakpoint\n");
+  return 0;
+}
+
+static int step(int argc, FAR char *argv[]) {
+  int i = 0;
+  printf("Next, World!!\n");
+  while (i < 10) {
+    printf("Step %d\n", i);
+    i++;
+  }
+  return 0;
+}
+
+static char *g_wild_pointer;
+static int print(int argc, FAR char *argv[]) {
+  static char g_buffer[64];
+  int i = 0;
+
+  g_wild_pointer = g_buffer;
+  snprintf(g_buffer, sizeof(g_buffer), "%d buffer\n", i);
+  while (i < 10) {
+    g_buffer[0] = '0' + i % 10;
+    printf("%s", g_buffer);
+    fflush(stdout);
+    i++;
+    sleep(1);
+  }
+  return 0;
+}
+
+static int watchpoint(int argc, FAR char *argv[]) {
+  printf("Watchpoint\n");
+  if (g_wild_pointer == NULL)
+    return 0;
+
+  strcpy(g_wild_pointer, "Wild pointer write.\n");
+  return 0;
+}
+
+nooptimiziation_function
+static int corruption(int argc, FAR char *argv[]) {
+  printf("Corruption\n");
+  int *p = (int *)malloc(1024);
+  for (int i = 0; i < 12; i++)
+    {
+      p[-i] = 0xdeadbeef;
+    }
+
+  return 0;
+}
+
+nooptimiziation_function
+static int leak(int argc, FAR char *argv[]) {
+  size_t size = rand() % 1024;
+  int *p = (int *)malloc(size);
+  printf("Leak: %p: %zu\n", p, size);
+  return 0;
+}
+
+#define ADD_COMMAND(name) {#name, name}
+
+static const struct {
+  const char *cmd;
+  int (*func)(int argc, FAR char *argv[]);
+} g_commands[] = {
+    ADD_COMMAND(hello),
+    ADD_COMMAND(breakpoint),
+    ADD_COMMAND(step),
+    ADD_COMMAND(print),
+    ADD_COMMAND(watchpoint),
+    ADD_COMMAND(corruption),
+    ADD_COMMAND(leak),
+};
+
+int main(int argc, FAR char *argv[]) {
+  int i;
+  if (argc < 2) {
+    printf("Usage: %s <command>\n", argv[0]);
+    return 1;
+  }
+
+  for (i = 0; i < sizeof(g_commands) / sizeof(g_commands[0]); i++) {
+    if (strcmp(argv[1], g_commands[i].cmd) == 0) {
+      return g_commands[i].func(argc, argv);
+    }
+  }
+
+  printf("Unknown command: %s\n", argv[1]);
   return 0;
 }
