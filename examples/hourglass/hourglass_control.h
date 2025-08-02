417da1e3de214f807b3fd5c259b3ea5dadb0bc05
p-liangshuang11 <your_name@xiaomi.com>
2025-08-03 04:09:17 +0800

    openvela中开发电子沙漏demo

---
+++ examples/hourglass/hourglass_control.h
@@ -0,0 +1,10 @@
+#ifndef HOURGLASS_CONTROL_H
+#define HOURGLASS_CONTROL_H
+
+#include <stdio.h>
+#include <time.h>
+#include "lvgl.h"
+
+void reset_sand_hourglass(void);
+
+#endif
---
+++ examples/hourglass/hourglass_page.h
@@ -0,0 +1,14 @@
+#ifndef HOURGLASS_PAGE_H
+#define HOURGLASS_PAGE_H
+
+
+
+
+#include <string.h>
+#include <stdio.h>
+#include "lvgl.h"
+#include "hourglass_control.h"
+
+void sandglass_page_create(void);
+
+#endif
